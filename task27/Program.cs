50695c22281f87097bc15e71c6bf7df54661eb2f
Anna Bolotova <anechkamilaha69@gmail.com>
2023-02-23 12:06:26 +0300

    was done task27 without functions

---
+++ task27/Program.cs
@@ -0,0 +1,11 @@
+﻿//  Напишите программу, которая принимает на вход число 
+//  и выдаёт сумму цифр в числе.
+System.Console.WriteLine("введите ваше число");
+string number = Console.ReadLine();
+int length = number.Length;
+int sum = 0;
+  for (int i=0; i<length; i++)
+ {
+      sum = sum + Convert.ToInt32(Convert.ToString(number[i]));
+ }
+  System.Console.WriteLine(sum);
